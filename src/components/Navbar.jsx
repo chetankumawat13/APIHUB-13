7b425a6dd573d6b179d20067a55cc4006e202b54
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-02-01 15:42:26 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between p-3 bg-[#bdf7b7] '>
-        <NavLink 
+    <div className='flex items-center justify-between p-3 bg-[#bdf7b7]'>
+      
+      <NavLink 
         to="/" 
-        className={({ isActive }) => 
-          `text-2xl font-bold cursor-pointer ${isActive ? 'text-blue-700' : 'text-[#020617]'}`
+        className={({ isActive }) =>
+          `text-2xl font-bold cursor-pointer ${
+            isActive ? 'text-blue-700' : 'text-[#020617]'
+          }`
         }
       >
         Hub-13
       </NavLink>
-        <nav>
-            <ul className='flex space-x-4'>
-                <li><NavLink to="/about">
-                        About
-                    </NavLink>
-                    </li>
-                <li>
-                    <NavLink to="services" >
-                        Services
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="contact" >
-                        Contact
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/AddNewApi" className=" text-black">
-                        Add New API
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+
+      <nav>
+        <ul className='flex space-x-4'>
+          
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/services">Services</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/AddNewApi" className="text-black">
+              Add New API
+            </NavLink>
+          </li>
+
+        </ul>
+      </nav>
     </div>
   )
 }
